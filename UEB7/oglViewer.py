89987a48de27ebc7e49df6f6d7b3c4d276368e3c
domeej <vyruza@freenet.de>
2017-06-07 14:06:55 +0200

    started with OpenGL Renderer: UEB7 A1 (Abgabe)

--- UEB7/oglViewer.py
+++ UEB7/oglViewer.py
@@ -8,7 +8,6 @@ data = []
 my_vbo = None
 
 def readOBJ(filename):
-
     vertices = []
     vertexnormals = []
     faces = []
@@ -40,27 +39,11 @@ def initGeometryfromFile():
 
     data = []
 
-    # Create BoundingBox
-    boundingBox = [map(min, zip(*vertices)), map(max, zip(*vertices))]
-    # Calc center of bounding box
-    center = [(x[0] + x[1]) / 2.0 for x in zip(*boundingBox)]
-    # Calc scale factor
-    scaleFactor = 2.0 / max([(x[1] - x[0]) for x in zip(*boundingBox)])
+    points = [[math.cos(i *math.pi/3),\
+               math.sin(i *math.pi / 3 )] \
+                for i in range(6) ]
 
-    # get the right data for the vbo
-    for vertex in faces:
-        vn = int(vertex[0]) - 1
-        nn = int(vertex[2]) - 1
-        if vertexnormals:
-            data.append(vertices[vn] + vertexnormals[nn])
-        else:
-            # calc standard normals, if no objectNormals available
-            normals = [x - y for x, y in zip(vertices[vn], center)]
-            l = math.sqrt(normals[0] ** 2 + normals[1] ** 2 + normals[2] ** 2)
-            normals = [x / l for x in normals]
-            data.append(vertices[vn] + normals)
-
-    my_vbo = vbo.VBO(array(data, 'f'))
+    my_vbo = vbo.VBO(array(points, 'f'))
 
 
 
@@ -77,7 +60,7 @@ def initGL(width, height):
 
 
 def display():
-
+    #global my_vbo
     # Clear frame buffer
     glClear(GL_COLOR_BUFFER_BIT)
     # Set color to light gray
@@ -86,18 +69,16 @@ def display():
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
     # Set primitive type to polygon
 
-    vbo.bind()
+    my_vbo.bind()
 
-    glVertexPointerf(vbo)
+    glVertexPointerf(my_vbo)
     glEnableClientState(GL_VERTEX_ARRAY)
-    glDrawArrays(GL_POLYGON, 0, len(data))
+    glDrawArrays(GL_POLYGON, 0, 6)
 
-    vbo.unbind()
+    my_vbo.unbind()
 
 
     glDisableClientState(GL_VERTEX_ARRAY)
-
-
     # Flush commands
     glFlush()
 
